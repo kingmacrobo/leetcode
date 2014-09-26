040c410917761ddcca304bb29078b5415faba789
QilongZhang <speknight4534@gmail.com>
2014-09-26 20:21:12 +0800

    错把我的Trapping_Rain_Water.cpp提交了

--- C_plus_plus/Trapping_Rain_Water.cpp
+++ C_plus_plus/Trapping_Rain_Water.cpp
@@ -1,38 +1,47 @@
-class Solution {
+/*************************************************************************
+	> File Name: Trapping_Rain_Water.cpp
+	> Author: QilongZhang
+	> Mail: Speknight4534@gmail.com
+	> Created Time: 2014年09月26日 星期五 10时53分01秒
+ ************************************************************************/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution{
 public:
-    int trap(int A[], int n) {
-        vector<int> max;
-        int result = 0;
-        if(n <= 1 ) return 0;
-        for(int i = 0 ; i < n ; ++i){
-            if(i == 0){ if(A[i]>A[i+1]) max.push_back(i); }
-            else if(i == n-1){ if(A[i]>A[i-1]) max.push_back(i); }
-            else{ if(A[i]>=A[i-1] && A[i]>=A[i+1]) max.push_back(i); }
+    int trap(int A[], int n){
+        vector<int> S(n), E(n);
+        int ret=0, pt=0;
+        for(pt=0; pt<n; ++pt){
+            if(pt==0){
+                S[pt]=A[pt];
+            }
+            else{
+                S[pt]=max(S[pt-1], A[pt]);
+            }   
         }
-        if(max.size()<=1) return 0;
-        int start=0,last=0,max2=-1,maxloc=-1;
-        for(int i = 1 ; i < max.size(); ++i){
-            if(max2<A[max[i]]){
-                max2 = A[max[i]];
-                maxloc = i;
+        for(pt=n-1; pt>=0; --pt){
+            if(pt==n-1){
+                E[pt]=A[pt];
             }
-            if(A[max[start]]<=A[max[i]]){
-                last = i;
-                for(int j = max[start]+1 ; j <= max[last]-1; ++j) 
-                    result += (A[max[start]] - A[j])>0?(A[max[start]] - A[j]):0;
-                start = i;
-                max2 = -1;
-                maxloc = -1;
-                continue;
-            }
-            if(i == max.size()-1){
-                for(int j = max[start]+1 ; j <= max[maxloc]-1; ++j)
-                    result += (A[max[maxloc]] - A[j])>0?(A[max[maxloc]] - A[j]):0;
-                last = i = start = maxloc;
-                max2 = -1;
-                maxloc = -1;
+            else{
+                E[pt]=max(E[pt+1], A[pt]);
             }
         }
-        return result;
+        for(pt=0; pt<n; ++pt){
+            ret+=min(S[pt], E[pt])-A[pt];
+        }
+        return ret;
     }
 };
+
+int main(){
+    int A[]={0,1,0,2,1,0,1,3,2,1,2,1};
+    Solution st;
+    cout<<st.trap(A, 12);
+    return 0;
+}
