76f747dc1b36d68577cbb231fc69f8f797207dff
Hongbo Wang <283613019@qq.com>
2014-10-10 13:17:43 +0800

    using "&" instead of "&&", it runs faster.

--- C_plus_plus/Same_Tree.cpp
+++ C_plus_plus/Same_Tree.cpp
@@ -16,6 +16,6 @@ public:
         }
         if(q==NULL) return false;
         if(p->val!=q->val) return false;
-        return isSameTree(p->left,q->left)&&isSameTree(p->right,q->right);
+        return isSameTree(p->left,q->left)&isSameTree(p->right,q->right);
     }
 };
