399da5df929fa3cbed06f416c16adf0235c1b7dc
Hongbo Wang <283613019@qq.com>
2015-05-06 16:09:03 +0800

    Create Single_Number.java

---
+++ java/Single_Number.java
@@ -0,0 +1,10 @@
+// use XOR to find the answer!
+public class Solution {
+    public int singleNumber(int[] nums) {
+        int result = nums[0];
+        for (int i = 1 ; i < nums.length ; ++i) {
+            result ^= nums[i] ;
+        }
+        return result ;
+    }
+}
