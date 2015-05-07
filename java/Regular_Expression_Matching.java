4b8d3d5ca7d4bd2c38858a79267ac754d39ba74c
Hongbo Wang <283613019@qq.com>
2015-05-07 15:10:34 +0800

    Update Regular_Expression_Matching.java

--- java/Regular_Expression_Matching.java
+++ java/Regular_Expression_Matching.java
@@ -12,24 +12,17 @@ public boolean isMatch(String s, String p) {
 		return dp(s, p, 0, 0, record) == 1;
 	}
 	public int dp(String s, String p, int i, int j, int[][] record) {
-		if (i < s.length() && j == p.length())
-			return 0;
+		if (i < s.length() && j == p.length()) return 0;
 		if (i == s.length() && j < p.length()) {
-			if (p.charAt(j) == '*') {
-				++j;
-			}
+			if (p.charAt(j) == '*')  ++j;
 			while (j < p.length()) {
-				if (j + 1 >= p.length() || p.charAt(j + 1) != '*') {
-					return 0;
-				}
+				if (j + 1 >= p.length() || p.charAt(j + 1) != '*') return 0;
 				j += 2;
 			}
 			return 1;
 		}
-		if (i == s.length() && j == p.length())
-			return 1;
-		if (record[i][j] != -1)
-			return record[i][j];
+		if (i == s.length() && j == p.length()) return 1;
+		if (record[i][j] != -1) return record[i][j];
 		if (s.charAt(i) == p.charAt(j)) {
 			if (j + 1 < p.length() && p.charAt(j + 1) == '*') {
 				if (dp(s, p, i, j + 2, record) == 1)
@@ -39,8 +32,7 @@ public boolean isMatch(String s, String p) {
 		}
 		if (p.charAt(j) == '*') {
 			int k = i;
-			if (k < 1)
-				return record[i][j] = 0;
+			if (k < 1) return record[i][j] = 0;
 			while (k < s.length() && s.charAt(k) == s.charAt(k - 1)) {
 				if (dp(s, p, k, j + 1, record) == 1)
 					return record[i][j] = 1;
@@ -51,17 +43,14 @@ public boolean isMatch(String s, String p) {
 		if (p.charAt(j) == '.') {
 			if (j + 1 < p.length() && p.charAt(j + 1) == '*') {
 				for (int k = i + 1; k <= s.length(); ++k) {
-					if (dp(s, p, k, j + 2, record) == 1) {
+					if (dp(s, p, k, j + 2, record) == 1) 
 						return record[i][j] = 1;
-					}
 				}
-			} else {
+			} else 
 				return record[i][j] = dp(s, p, i + 1, j + 1, record);
-			}
 		}
-		if (j + 1 < p.length() && p.charAt(j + 1) == '*') {
+		if (j + 1 < p.length() && p.charAt(j + 1) == '*') 
 			return record[i][j] = dp(s, p, i, j + 2, record);
-		}
 		return record[i][j] = 0;
 	}
 }
