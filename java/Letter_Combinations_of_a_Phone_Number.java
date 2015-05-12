5b40a8c23e33c1a939bec2398c8a754bbed336fa
Hongbo Wang <283613019@qq.com>
2015-05-12 14:11:10 +0800

    Update Letter_Combinations_of_a_Phone_Number.java

--- java/Letter_Combinations_of_a_Phone_Number.java
+++ java/Letter_Combinations_of_a_Phone_Number.java
@@ -2,21 +2,19 @@
 // time is 3^n , space is 3^n
 public class Solution {
     public List<String> letterCombinations(String digits) {
-        String[][] table = {{""},{""},{"a","b","c"},{"d","e","f"},
-                            {"g","h","i"},{"j","k","l"},{"m","n","o"},
-                            {"p","q","r","s"},{"t","u","v"},{"w","x","y","z"}};
+        String[] table = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
         if (digits.length()==0) return new ArrayList<String>();
         return dp(digits,0,table);
     }
-    public List<String> dp(String digits, int i,String[][] table) {
+    public List<String> dp(String digits, int i,String[] table) {
         if (i == digits.length()) return Arrays.asList("");
         int index = digits.charAt(i) - '0';
         List<String> result = new ArrayList<String>();
         List<String> after = dp(digits,i+1,table);
-        for (int j = 0 ; j < table[index].length ; ++j) {
+        for (int j = 0 ; j < table[index].length() ; ++j) {
             for (String s : after) {
                 StringBuffer temp = new StringBuffer();
-                temp.append(table[index][j]+s);
+                temp.append(table[index].charAt(j)+s);
                 result.add(temp.toString());
             }
         }
