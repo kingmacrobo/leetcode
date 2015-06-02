80995676063f0077de4ab6f5ec26fecd443900cf
Hongbo Wang <283613019@qq.com>
2015-06-02 14:02:03 +0800

    Update Add_Binary.java

--- java/Add_Binary.java
+++ java/Add_Binary.java
@@ -23,3 +23,21 @@ public class Solution {
         return sb.toString();
     }
 }
+
+// It's interesting to simplify the code like this :
+
+public class Solution {
+    public String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        int i = a.length()-1, j = b.length()-1;
+        int carry = 0;
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int sum = carry + 
+                  (i < 0 ? 0 : a.charAt(i--)-'0') + 
+                  (j < 0 ? 0 : b.charAt(j--)-'0');
+            sb.insert(0, (char)(sum % 2 + '0'));
+            carry = sum / 2;
+        }
+        return sb.toString();
+    }
+}
