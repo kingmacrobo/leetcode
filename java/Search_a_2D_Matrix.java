a0acfa5409f686f3aaa8b7d3b738160f0f08df65
Hongbo Wang <283613019@qq.com>
2015-06-03 22:21:49 +0800

    Update Search_a_2D_Matrix.java

--- java/Search_a_2D_Matrix.java
+++ java/Search_a_2D_Matrix.java
@@ -18,3 +18,18 @@ public class Solution {
         return false;
     }
 }
+
+// Another method using O(m+n) time:
+// search from the up right point, if it is greater than target , go left, if it is less than target, go down, or equal, we find it!
+
+public class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int i = 0, j = matrix[0].length-1;
+        while (i < matrix.length && j >= 0) {
+            if (matrix[i][j] == target) return true;
+            if (matrix[i][j] < target) ++i;
+            else --j;
+        }
+        return false;
+    }
+}
