c99404b7968c9f2a86d63a89c0e054deeeac888e
Hongbo Wang <283613019@qq.com>
2015-06-03 15:40:58 +0800

    Create Simplify_Path.java

---
+++ java/Simplify_Path.java
@@ -0,0 +1,33 @@
+// Unix path
+// We just record the valid words and raze the tail of the words list when come across "../"
+// Finally we rebuild the path by add "/"+words[i] to the new Path.
+// Time is O(n), Space is O(n)
+
+public class Solution {
+    public String simplifyPath(String path) {
+        List<String> list = new LinkedList<String>();
+        int i = 0;
+        while (i < path.length()) {
+            if (path.charAt(i) == '/') {
+                i++;
+                continue;
+            }
+            StringBuilder sb = new StringBuilder();
+            while (i < path.length() && path.charAt(i) != '/') {
+                sb.append(path.charAt(i++));
+            }
+            if (sb.length() == 1 && sb.charAt(0) == '.') continue;
+            else if (sb.length() == 2 && sb.charAt(0) == '.' && sb.charAt(1) == '.') {       
+                if (list.size() > 0) list.remove(list.size()-1);
+            }
+            else if (sb.length() != 0)
+                list.add(sb.toString());
+        }
+        StringBuilder sb = new StringBuilder();
+        for (String s : list) 
+            sb.append('/' + s);
+        if (list.size() == 0 && path.length() != 0)
+            return new String("/");
+        return sb.toString();
+    }
+}
