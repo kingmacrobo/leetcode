92957665b1de01ea160eb7faf105c7bed8c59caf
Hongbo Wang <283613019@qq.com>
2015-06-08 15:08:45 +0800

    Update Maximal_Rectangle.java

--- java/Maximal_Rectangle.java
+++ java/Maximal_Rectangle.java
@@ -4,7 +4,7 @@
 //    we define a submax(i,j), 
 //    if matrix[i][j] == '1', we research the submax rectangle containing point (i,j), and set the submax to be the submax(i,j) val.
 //    else we set the submax to be 0.
-/     Then, the recursive formula can be this :
+//     Then, the recursive formula can be this :
 //        dp(i,j) = max {submax(i,j), max {dp(i+1,j), dp(i,j+1)}}
 // Time is O(n^4), space is O(n^2).
 
@@ -40,3 +40,44 @@ public class Solution {
         return record[x][y] = Math.max(submax, Math.max(dp(x+1,y,m,n,matrix,record),dp(x,y+1,m,n,matrix,record))); 
     }
 }
+
+
+// Another greate solution by using the Largest Rectangle in Histogram's method
+// What we should do is to scan row by row to update the height of the Histogram and find the larget rectangle in each row based.
+// Time is O(n^2), space is O(n).
+
+public class Solution {
+    public int maximalRectangle(char[][] matrix) {
+        if (matrix==null||matrix.length==0||matrix[0].length==0)
+            return 0;
+        int cLen = matrix[0].length;    // column length
+        int rLen = matrix.length;       // row length
+        // height array 
+        int[] h = new int[cLen+1];
+        h[cLen]=0;
+        int max = 0;
+
+
+        for (int row=0;row<rLen;row++) {
+            Stack<Integer> s = new Stack<Integer>();
+            for (int i=0;i<cLen+1;i++) {
+                if (i<cLen)
+                    if(matrix[row][i]=='1')
+                        h[i]+=1;
+                    else h[i]=0;
+
+                if (s.isEmpty()||h[s.peek()]<=h[i])
+                    s.push(i);
+                else {
+                    while(!s.isEmpty()&&h[i]<h[s.peek()]){
+                        int top = s.pop();
+                        int area = h[top]*(s.isEmpty()?i:(i-s.peek()-1));
+                        if (area>max)
+                            max = area;
+                    }
+                    s.push(i);
+                }
+            }
+        }
+        return max;
+    }
