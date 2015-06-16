b0c3237aef49fe8fbfeed037fe69526dda46e145
Hongbo Wang <283613019@qq.com>
2015-06-16 11:11:16 +0800

    Update Construct_Binary_Tree_from_Preoder_and_Inorder_Traversal.java

--- java/Construct_Binary_Tree_from_Preoder_and_Inorder_Traversal.java
+++ java/Construct_Binary_Tree_from_Preoder_and_Inorder_Traversal.java
@@ -32,3 +32,23 @@ public class Solution {
         return node;
     }
 }
+
+// We can accelerate the process by using index, other than the LinkedList.
+// Time is also O(n^2), space is O(n).
+
+public class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return constructTree(preorder,0, inorder, 0, inorder.length-1);
+    }
+    public TreeNode constructTree(int[] pre, int index, int[] inorder, int start, int end) {
+        if (start > end) return null;
+        TreeNode node = new TreeNode(pre[index]);
+        int i = start;
+        for (; i <= end; ++i)
+            if (inorder[i] == pre[index])
+                break;
+        node.left = constructTree(pre,index+1, inorder, start, i-1);
+        node.right = constructTree(pre,index+i-start+1,inorder, i+1, end);
+        return node;
+    }
+}
