9de455ba47a8d05d8f76923c442568f8f9680c50
Hongbo Wang <283613019@qq.com>
2015-06-16 10:32:25 +0800

    Update Maximum_Depth_of_Binary_Tree.java

--- java/Maximum_Depth_of_Binary_Tree.java
+++ java/Maximum_Depth_of_Binary_Tree.java
@@ -1,7 +1,27 @@
+// DFS
 // Just one line code !
 // Time is O(n), space is O(n);
 public class Solution {
     public int maxDepth(TreeNode root) {
         return root == null ? 0 : Math.max(maxDepth(root.left),maxDepth(root.right))+1;
+    }
+}
+
+// BFS
+// Time is O(n), space is O(n).
+public class Solution {
+    public int maxDepth(TreeNode root) {
+        Queue<TreeNode> que = new LinkedList<TreeNode>();
+        que.offer(root);
+        int depth = 0;
+        while (que.peek() != null) {
+            depth++;
+            for (int n = que.size(); n > 0 ; n--) {
+                TreeNode node = que.poll();
+                if (node.left != null) que.offer(node.left);
+                if (node.right != null) que.offer(node.right);
+            }
+        }
+        return depth;
     }
 }
