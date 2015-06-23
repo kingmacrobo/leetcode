3a705b8f35f17838b4c147e91e31d4b35cd66c1f
Hongbo Wang <283613019@qq.com>
2015-06-23 11:45:40 +0800

    Update Populating_Next_Right_Points_in_Each_Node.java

--- java/Populating_Next_Right_Points_in_Each_Node.java
+++ java/Populating_Next_Right_Points_in_Each_Node.java
@@ -24,3 +24,57 @@ public class Solution {
         }
     }
 }
+
+// We can also solve this problem in O(1) space :
+// The method below can suit any binary tree.
+
+/**
+ * Definition for binary tree with next pointer.
+ * public class TreeLinkNode {
+ *     int val;
+ *     TreeLinkNode left, right, next;
+ *     TreeLinkNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public void connect(TreeLinkNode root) {
+        TreeLinkNode p = root, first = null, pre = null;;
+        int curCount = 1, nextCount = 0, cur = 0;
+        while (p != null) {
+            if (p.left != null) {
+                if (pre == null) {
+                    pre = p.left;
+                    first = p.left;
+                }
+                else {
+                    pre.next = p.left;
+                    pre = p.left;
+                }
+                nextCount++;
+            }
+            if (p.right != null) {
+                if (pre == null) {
+                    pre = p.right;
+                    first = p.right;
+                }
+                else {
+                    pre.next = p.right;
+                    pre = p.right;
+                }
+                nextCount++;
+            }
+            
+            if (++cur == curCount) {
+                p.next = null;
+                cur = 0;
+                curCount = nextCount;
+                nextCount = 0;
+                p = first;
+                first = pre = null;
+            }
+            else
+                p = p.next;
+            
+        }
+    }
+}
