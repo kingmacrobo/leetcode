ea3d63e7cc5953cd97ddc8dc0ebb8e235515b90c
Hongbo Wang <283613019@qq.com>
2015-06-29 19:22:52 +0800

    Update Palindrome_Partitioning.java

--- java/Palindrome_Partitioning.java
+++ java/Palindrome_Partitioning.java
@@ -60,3 +60,42 @@ public class Solution {
         return result;
     }
 }
+
+// Other's concise dp solution :
+
+public class Solution {
+    public List<List<String>> partition(String s) {
+    int len = s.length();
+        List<List<String>>[] result = new List[len + 1];
+        result[0] = new ArrayList<List<String>>();
+        result[0].add(new ArrayList<String>());
+
+        boolean[][] pair = new boolean[len][len];
+        for(int i=0; i<s.length(); i++){
+            result[i+1] = new ArrayList<List<String>>();
+            char c = s.charAt(i);
+            for(int j=0; j<=i; j++){
+                if(j == i)
+                    pair[j][i] = true;
+                else{
+                    if(s.charAt(j) != c)
+                        continue;
+                    if(j == i-1)
+                        pair[j][i] = true;
+                    else
+                        pair[j][i] = pair[j+1][i-1];
+                }
+                if(pair[j][i]){
+                    String str = s.substring(j, i+1);
+                    for(List<String> r : result[j]){
+                        List<String> ri = new ArrayList<String>(r);
+                        ri.add(str);
+                        result[i+1].add(ri);
+                    }
+                }
+            }
+        }
+
+        return result[len];        
+    }
+}
