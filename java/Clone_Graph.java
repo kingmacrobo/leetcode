fab7b66623d711daf633fea4bcfd45dee5293f20
Hongbo Wang <283613019@qq.com>
2015-06-30 10:52:08 +0800

    Create Clone_Graph.java

---
+++ java/Clone_Graph.java
@@ -0,0 +1,40 @@
+// BFS + HashMap
+// Firstly, using bfs to create all new node.
+// And then, add neighbors to each node created via Hashmap.
+// Time is O(n), space is O(n).
+
+/**
+ * Definition for undirected graph.
+ * class UndirectedGraphNode {
+ *     int label;
+ *     List<UndirectedGraphNode> neighbors;
+ *     UndirectedGraphNode(int x) { label = x; neighbors = new ArrayList<UndirectedGraphNode>(); }
+ * };
+ */
+public class Solution {
+    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+        if (node == null) return null;
+        HashMap<UndirectedGraphNode, UndirectedGraphNode> table = new HashMap<UndirectedGraphNode, UndirectedGraphNode>();
+        Set<UndirectedGraphNode> visited = new HashSet<UndirectedGraphNode>();
+        Queue<UndirectedGraphNode> que = new LinkedList<UndirectedGraphNode>();
+        que.offer(node);
+        visited.add(node);
+        table.put(node, new UndirectedGraphNode(node.label));
+        while (que.peek() != null) {
+            UndirectedGraphNode p = que.poll();
+            for (UndirectedGraphNode n : p.neighbors) {
+                if (! visited.contains(n)) {
+                    table.put(n, new UndirectedGraphNode(n.label));
+                    que.offer(n);
+                    visited.add(n);
+                }
+            }
+        }
+        for (Map.Entry<UndirectedGraphNode, UndirectedGraphNode> entry : table.entrySet()) {
+            for (UndirectedGraphNode n : entry.getKey().neighbors) {
+                entry.getValue().neighbors.add(table.get(n));
+            }
+        }
+        return table.get(node);
+    }
+}
