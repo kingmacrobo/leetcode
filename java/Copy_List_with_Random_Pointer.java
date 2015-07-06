92fb065f543e0ba59b8e0506757142e53464e444
Hongbo Wang <kingmacrobo@gmail.com>
2015-07-06 15:30:52 +0800

    Update Copy_List_with_Random_Pointer.java

--- java/Copy_List_with_Random_Pointer.java
+++ java/Copy_List_with_Random_Pointer.java
@@ -36,3 +36,44 @@ public class Solution {
         return first;
     }
 }
+
+// clone the new node just after the origin node.
+// and then linked the new nodes's random node
+// finnaly, recover the origin node list and generate the deep copy list.
+// Time is O(n), space is O(n).
+
+/**
+ * Definition for singly-linked list with a random pointer.
+ * class RandomListNode {
+ *     int label;
+ *     RandomListNode next, random;
+ *     RandomListNode(int x) { this.label = x; }
+ * };
+ */
+public class Solution {
+    public RandomListNode copyRandomList(RandomListNode head) {
+        RandomListNode p = head, result = null;
+        if (head == null) return null;
+        while (p != null) {
+            RandomListNode after = p.next;
+            p.next = new RandomListNode(p.label);
+            p.next.next = after;
+            p = after;
+        }
+        p = head;
+        while (p != null) {
+            p.next.random = p.random == null ? null : p.random.next;
+            p = p.next.next;
+        }
+        result = head.next;
+        p = head;
+        while (p.next.next != null) {
+            RandomListNode temp = p.next.next;
+            p.next.next = p.next.next.next;
+            p.next = temp;
+            p = temp;
+        }
+        p.next = null;
+        return result;
+    }
+}
