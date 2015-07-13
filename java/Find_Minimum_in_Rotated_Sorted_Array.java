cadabe647b7889efee319b92479154496c280381
Hongbo Wang <kingmacrobo@gmail.com>
2015-07-13 10:19:18 +0800

    Update Find_Minimum_in_Rotated_Sorted_Array.java

--- java/Find_Minimum_in_Rotated_Sorted_Array.java
+++ java/Find_Minimum_in_Rotated_Sorted_Array.java
@@ -20,3 +20,17 @@ public class Solution {
         return nums[low];
     }
 }
+
+// we can simplized the code like this :
+// we just compare mid and high and decided which sub array containing the minimum value.
+public class Solution {
+    public int findMin(int[] nums) {
+        int low = 0, high = nums.length-1;
+        while (low < high) {
+            int mid = (low+high)/2;
+            if (nums[mid] < nums[high]) high = mid;
+            else low = mid + 1;
+        }
+        return nums[low];
+    }
+}
