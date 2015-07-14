4d26548c68a43e1bd91b61ee41d43b9b75ab1897
Hongbo Wang <kingmacrobo@gmail.com>
2015-07-14 21:46:39 +0800

    Update Dungeon_Game.java

--- java/Dungeon_Game.java
+++ java/Dungeon_Game.java
@@ -25,3 +25,33 @@ public class Solution {
         return dp[0][0];
     }
 }
+
+//We can reduce the dp space to O(n) by using rolling array!
+
+public class Solution {
+    public int calculateMinimumHP(int[][] dungeon) {
+        int m = dungeon.length, n = dungeon[0].length;
+        int[] current = new int[n];
+        int[] pre = new int[n];
+        for (int i = m-1; i >= 0; --i) {
+            for (int j = n-1; j >= 0; --j) {
+                if (i == m-1 && j == n-1) {
+                    current[j] = dungeon[i][j] >= 0 ? 1 : 1-dungeon[i][j];
+                }
+                else if (i == m-1) {
+                    current[j] = Math.max(current[j+1] - dungeon[i][j], 1);
+                }
+                else if (j == n-1) {
+                    current[j] = Math.max(pre[j] - dungeon[i][j], 1);
+                }
+                else {
+                    current[j] = Math.min(Math.max(pre[j]-dungeon[i][j],1), Math.max(current[j+1]-dungeon[i][j],1));
+                }
+            }
+            int[] temp = pre;
+            pre = current;
+            current = pre;
+        }
+        return pre[0];
+    }
+}
