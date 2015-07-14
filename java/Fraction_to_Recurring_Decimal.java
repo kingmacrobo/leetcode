d774225e3f93b22c3d65ffa10dcb0342b6c61cdf
Hongbo Wang <kingmacrobo@gmail.com>
2015-07-14 16:08:22 +0800

    Update Fraction_to_Recurring_Decimal.java

--- java/Fraction_to_Recurring_Decimal.java
+++ java/Fraction_to_Recurring_Decimal.java
@@ -46,3 +46,35 @@ public class Solution {
         return sb.toString();
     }
 }
+
+// We can make the code more concise:
+public class Solution {
+    public String fractionToDecimal(int numerator, int denominator) {
+        StringBuilder sb = new StringBuilder();
+        HashMap<Long, Integer> mods = new HashMap<Long, Integer>();
+        long a = Long.valueOf(numerator);
+        long b = Long.valueOf(denominator);
+        if ((a > 0) ^ (b > 0) && a*b != 0) sb.append('-');
+        a = Math.abs(a);
+        b = Math.abs(b);
+        sb.append(a/b);
+        long n = a%b;
+        if (n == 0) return sb.toString();
+        StringBuilder sbb = new StringBuilder();
+        sb.append('.');
+        while (n != 0) {
+            n *= 10;
+            if (mods.containsKey(n)) {
+                int loc = mods.get(n);
+                sbb.insert(loc,'(');
+                sbb.append(')');
+                break;
+            }
+            mods.put(n,sbb.length());
+            sbb.append(n/b);
+            n = n%b;
+        }
+        sb.append(sbb);
+        return sb.toString();
+    }
+}
