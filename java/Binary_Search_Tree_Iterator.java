90c3e10b213a940000914054f2dc74c17aac9416
Hongbo Wang <kingmacrobo@gmail.com>
2015-07-14 17:47:26 +0800

    Create Binary_Search_Tree_Iterator.java

---
+++ java/Binary_Search_Tree_Iterator.java
@@ -0,0 +1,46 @@
+// Using a stack to record the current minimum node.
+// Update the stack when pop one node.
+// Time is O(1) on each operater, space is O(h).
+
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+public class BSTIterator {
+
+    Stack<TreeNode> stack = new Stack<TreeNode>();
+    public BSTIterator(TreeNode root) {
+        while (root != null) {
+            stack.push(root);
+            root = root.left;
+        }
+    }
+
+    /** @return whether we have a next smallest number */
+    public boolean hasNext() {
+        return !stack.empty();
+    }
+
+    /** @return the next smallest number */
+    public int next() {
+        TreeNode node = stack.pop();
+        TreeNode right = node.right;
+        while (right != null) {
+            stack.push(right);
+            right = right.left;
+        }
+        return node.val;
+    }
+}
+
+/**
+ * Your BSTIterator will be called like this:
+ * BSTIterator i = new BSTIterator(root);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
