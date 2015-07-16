2b4c682553b3350128742bc34b73e57fd5214d79
Hongbo Wang <kingmacrobo@gmail.com>
2015-07-16 21:18:03 +0800

    Create Count_Primes.java

---
+++ java/Count_Primes.java
@@ -0,0 +1,20 @@
+// Prime method : Filter Method！------Sieve of Eratosthenes : https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+// Time todo , space O(n).
+
+public class Solution {
+    public int countPrimes(int n) {
+        boolean[] not = new boolean[n];
+        Arrays.fill(not, false);
+        int count = 0;
+        for (int i = 2; i < n; ++i) {
+            if (not[i]) continue;
+            count++;
+            int k = i*2;
+            while (k < n) {
+                not[k] = true;
+                k += i;
+            }
+        }
+        return count;
+    }
+}
