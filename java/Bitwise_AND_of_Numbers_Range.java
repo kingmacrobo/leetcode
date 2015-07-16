bd0d9ba67c54a29a6e916203d0c088fc39582f31
Hongbo Wang <kingmacrobo@gmail.com>
2015-07-16 19:15:51 +0800

    Update Bitwise_AND_of_Numbers_Range.java

--- java/Bitwise_AND_of_Numbers_Range.java
+++ java/Bitwise_AND_of_Numbers_Range.java
@@ -30,3 +30,9 @@ public int rangeBitwiseAnd(int m, int n) {
         }
         return m<<=count;
     }
+    
+// Also an awesome solution !
+
+int rangeBitwiseAnd(int m, int n) {
+    return (n > m) ? (rangeBitwiseAnd(m/2, n/2) << 1) : m;
+}
