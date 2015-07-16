abd2bc23703c9622fc1c9b39902be8cdc4edc412
Hongbo Wang <kingmacrobo@gmail.com>
2015-07-16 19:30:26 +0800

    Update Happy_Number.java

--- java/Happy_Number.java
+++ java/Happy_Number.java
@@ -17,3 +17,28 @@ public class Solution {
         return isHappy(sum);
     }
 }
+
+
+// There is a magic solution USING RING FAST SLOW METHOD JUST LIKE THE CYCLE IN A LINKED LIST!!!!
+
+int digitSquareSum(int n) {
+    int sum = 0, tmp;
+    while (n) {
+        tmp = n % 10;
+        sum += tmp * tmp;
+        n /= 10;
+    }
+    return sum;
+}
+
+bool isHappy(int n) {
+    int slow, fast;
+    slow = fast = n;
+    do {
+        slow = digitSquareSum(slow);
+        fast = digitSquareSum(fast);
+        fast = digitSquareSum(fast);
+    } while(slow != fast);
+    if (slow == 1) return 1;
+    else return 0;
+}
