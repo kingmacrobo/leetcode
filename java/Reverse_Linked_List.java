d7a92688f951ea2884fcc8b1683a3540238a4e95
Hongbo Wang <kingmacrobo@gmail.com>
2015-07-17 10:47:53 +0800

    Update Reverse_Linked_List.java

--- java/Reverse_Linked_List.java
+++ java/Reverse_Linked_List.java
@@ -13,3 +13,24 @@ public class Solution {
         return pre;
     }
 }
+
+// Recursively
+// Time is O(n), space is O(n).
+
+public class Solution {
+    ListNode result = null;
+    public ListNode reverseList(ListNode head) {
+        if (head == null) return null;
+        reverse(head).next = null;
+        return result;
+    }
+    public ListNode reverse(ListNode head) {
+        if (head.next == null) {
+            result = head;
+            return head;
+        }
+        ListNode after = reverse(head.next);
+        after.next = head;
+        return head;
+    }
+}
