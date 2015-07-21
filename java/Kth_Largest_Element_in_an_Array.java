01a1cb8b1782a0d873892a19733be20368c99f04
Hongbo Wang <kingmacrobo@gmail.com>
2015-07-21 15:23:12 +0800

    Update Kth_Largest_Element_in_an_Array.java

--- java/Kth_Largest_Element_in_an_Array.java
+++ java/Kth_Largest_Element_in_an_Array.java
@@ -32,3 +32,43 @@ public class Solution {
         return quickSort(nums, start, a-1, k);
     }
 }
+
+// HeapSort solution
+// Add each element to heap, and pop  k-1 times, and then the top of the heap is the answer.
+// Time is nlogn, space is O(1).
+
+public class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>(nums.length, new Comparator<Integer>(){
+            @Override
+            public int compare(Integer a, Integer b) {
+                if (a == b) return 0;
+                return a < b ? 1 : -1;
+            }
+        });
+        for (int i = 0; i < nums.length; ++i)
+            pq.offer(nums[i]);
+        for (int i = 0; i < k-1; ++i)
+            pq.poll();
+        return pq.poll();
+    }
+}
+
+// We can reduce the heapSort solution to O(nlogk) by using a k size min-heap.
+
+public class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>(k);
+        for (int i = 0; i < nums.length; ++i) {
+            if (pq.size() < k)
+                pq.offer(nums[i]);
+            else {
+                if (pq.peek() < nums[i]) {
+                    pq.poll();
+                    pq.offer(nums[i]);
+                }
+            }
+        }
+        return pq.peek();
+    }
+}
