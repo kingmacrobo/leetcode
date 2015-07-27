8378f203a74b4984a57ae416afff48fe57a5f236
Hongbo Wang <kingmacrobo@gmail.com>
2015-07-27 23:01:30 +0800

    Update Lowest_Common_Ancestor_of_a_Binary_Tree.java

--- java/Lowest_Common_Ancestor_of_a_Binary_Tree.java
+++ java/Lowest_Common_Ancestor_of_a_Binary_Tree.java
@@ -39,3 +39,14 @@ public class Solution {
         return 2;
     }
 }
+
+
+// Below is the best solution of LCA:
+// Time is O(n), space is O(n).
+
+public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    if (root == null || root == p || root == q) return root;
+    TreeNode left = lowestCommonAncestor(root.left, p, q);
+    TreeNode right = lowestCommonAncestor(root.right, p, q);
+    return left == null ? right : right == null ? left : root;
+}
