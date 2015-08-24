8d6ca82f11456de453450a15cfdd575b989f2183
Hongbo Wang <kingmacrobo@gmail.com>
2015-08-24 09:57:10 +0800

    Update Rotate_Array.java

--- java/Rotate_Array.java
+++ java/Rotate_Array.java
@@ -20,3 +20,25 @@ public class Solution {
         }
     }
 }
+
+// Reverse Array method
+// Time is O(n), space is O(1).
+
+public class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k %= n;
+        reverse(nums, 0, n - k - 1);
+        reverse(nums, n - k, n - 1);
+        reverse(nums, 0, n - 1);
+    }
+    public void reverse(int[] nums, int i, int j) {
+        while (i < j) {
+            int temp = nums[j];
+            nums[j] = nums[i];
+            nums[i] = temp;
+            ++i;
+            --j;
+        }
+    }
+}
