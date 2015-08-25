9f8272d27c82af400d74f26e6f6821056c5dbb94
Hongbo Wang <kingmacrobo@gmail.com>
2015-08-25 15:55:16 +0800

    Update Maximum_Product_Subarray.java

--- java/Maximum_Product_Subarray.java
+++ java/Maximum_Product_Subarray.java
@@ -91,3 +91,26 @@ public class Solution {
         return hasZero ? Math.max(max,0) : max;
     }
 }
+
+// The best solution :
+class Solution {
+ // author : s2003zy
+ // weibo : http://weibo.com/574433433
+ // blog : http://s2003zy.com
+ // Time : O(n)
+ // Space : O(1)
+ public:
+    int maxProduct(int A[], int n) {
+        int frontProduct = 1;
+        int backProduct = 1;
+        int ans = INT_MIN;
+        for (int i = 0; i < n; ++i) {
+            frontProduct *= A[i];
+            backProduct *= A[n - i - 1];
+            ans = max(ans,max(frontProduct,backProduct));
+            frontProduct = frontProduct == 0 ? 1 : frontProduct;
+            backProduct = backProduct == 0 ? 1 : backProduct;
+        }
+        return ans;
+    }
+};
