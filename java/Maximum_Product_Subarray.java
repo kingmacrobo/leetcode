73a47ea14aa37e923f8678363c2523b8deefdc9a
Hongbo Wang <kingmacrobo@gmail.com>
2015-08-25 16:02:20 +0800

    Update Maximum_Product_Subarray.java

--- java/Maximum_Product_Subarray.java
+++ java/Maximum_Product_Subarray.java
@@ -93,24 +93,17 @@ public class Solution {
 }
 
 // The best solution :
-class Solution {
- // author : s2003zy
- // weibo : http://weibo.com/574433433
- // blog : http://s2003zy.com
- // Time : O(n)
- // Space : O(1)
- public:
-    int maxProduct(int A[], int n) {
-        int frontProduct = 1;
-        int backProduct = 1;
-        int ans = INT_MIN;
+public class Solution {
+    public int maxProduct(int[] nums) {
+        int pre = 1, suf = 1, max = Integer.MIN_VALUE;
+        int n = nums.length;
         for (int i = 0; i < n; ++i) {
-            frontProduct *= A[i];
-            backProduct *= A[n - i - 1];
-            ans = max(ans,max(frontProduct,backProduct));
-            frontProduct = frontProduct == 0 ? 1 : frontProduct;
-            backProduct = backProduct == 0 ? 1 : backProduct;
+            pre *= nums[i];
+            suf *= nums[n-i-1];
+            max = Math.max(max, Math.max(pre,suf));
+            if (pre == 0) pre = 1;
+            if (suf == 0) suf = 1;
         }
-        return ans;
+        return max;
     }
-};
+}
