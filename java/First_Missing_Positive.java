e58ed925bd3c1bb7a106eb43d82cd02ed3cfebd1
Hongbo Wang <kingmacrobo@gmail.com>
2015-08-26 16:24:08 +0800

    Update First_Missing_Positive.java

--- java/First_Missing_Positive.java
+++ java/First_Missing_Positive.java
@@ -16,3 +16,31 @@ public class Solution {
         return nums.length+1;
     }
 }
+
+
+// Swap Sort
+// Put the nums[i] to the location of nums[i]-1.
+// And then scan once to find the first mismatch loc and return loc+1.
+// Time is O(n), space is O(1).
+
+public class Solution {
+    public int firstMissingPositive(int[] nums) {
+        for (int i = 0; i < nums.length; ++i) {
+            while (nums[i] != i+1 && nums[i] <= nums.length) {
+                if (nums[i] == i+1 || nums[i] <= 0) break;
+                int k = nums[i]-1;
+                if (nums[k] == k+1) break;
+                swap(nums, i, k);
+            }
+        }
+        for (int i = 0; i < nums.length; ++i) {
+            if (nums[i] != i+1) return i+1;
+        }
+        return nums.length+1;
+    }
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[j];
+        nums[j] = nums[i];
+        nums[i] = temp;
+    }
+}
