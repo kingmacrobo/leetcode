13c3fc2e70e323a3b9abf1ce4073de8e49001a38
Hongbo Wang <kingmacrobo@gmail.com>
2015-08-27 12:32:13 +0800

    Update Best_Time_to_Buy_and_Sell_Stock_II.java

--- java/Best_Time_to_Buy_and_Sell_Stock_II.java
+++ java/Best_Time_to_Buy_and_Sell_Stock_II.java
@@ -15,3 +15,17 @@ public class Solution {
         return profit;
     }
 }
+
+// We can simplize it like this:
+
+public class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices.length == 0) return 0;
+        int max = 0;
+        for (int i = 1; i < prices.length; ++i) {
+            if (prices[i] >= prices[i-1])
+                max += prices[i] - prices[i-1];
+        }
+        return max;
+    }
+}
