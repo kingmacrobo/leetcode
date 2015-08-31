0adacffcff4c6c2168dae844c9bc9976bab9cc71
Hongbo Wang <kingmacrobo@gmail.com>
2015-08-31 15:45:24 +0800

    Update Minimum_Window_Substring.java

--- java/Minimum_Window_Substring.java
+++ java/Minimum_Window_Substring.java
@@ -82,3 +82,74 @@ public class Solution {
 	        }
 	    }
 }
+
+// Another solution, Using Queue and HashMap
+// Time O(n).
+
+public class Solution {
+    public String minWindow(String s, String t) {
+        Node head = new Node(-1), tail = new Node(-1);
+        head.after = tail;
+        head.pre = tail.after = null;
+        tail.pre = head;
+        HashMap<Character, Integer> set = new HashMap<Character, Integer>();
+        HashMap<Character, Queue<Node>> table = new HashMap<Character, Queue<Node>>();
+        for (int i = 0; i < t.length(); ++i) {
+            char c = t.charAt(i);
+            if (!set.containsKey(c)) {
+                set.put(c, 1);
+            }
+            else set.put(c, set.get(c)+1);
+        }
+        int count = 0, start = -1, end = -1;
+        for (int i = 0; i < s.length(); ++i) {
+            char c = s.charAt(i);
+            if (!set.containsKey(c)) continue;
+            Node node = null;
+            if (!table.containsKey(c)) {
+                node = new Node(i);
+                Queue<Node> que = new LinkedList<Node>();
+                que.offer(node);
+                table.put(c, que);
+                count++;
+            }
+            else if (table.containsKey(c) && table.get(c).size() < set.get(c)) {
+                node = new Node(i);
+                table.get(c).offer(node);
+                count++;
+            }
+            else {
+                Queue<Node> que = table.get(c);
+                node = que.poll();
+                que.offer(node);
+                node.index = i;
+                node.pre.after = node.after;
+                node.after.pre = node.pre;
+            }
+            tail.pre.after = node;
+            node.pre = tail.pre;
+            node.after = tail;
+            tail.pre = node;
+            if (count == t.length()) {
+                int len = tail.pre.index - head.after.index + 1;
+                if (start == -1 || end-start+1 > len) {
+                    start = head.after.index;
+                    end= tail.pre.index;
+                }
+            }
+        }
+        if (start != -1) {
+            return s.substring(start, end+1);
+        }
+        return "";
+    }
+    
+    class Node {
+        int index;
+        Node pre;
+        Node after;
+        public Node(int index) {
+            this.index = index;
+        }
+    }
+}
